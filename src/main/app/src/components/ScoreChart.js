b644b9c61422ca523574ba318f6fb1c9206d7032
David Dellsperger <david.dellsperger@gmail.com>
2023-09-03 23:25:00 -0500

    Add initial chart with data from first game

---
+++ src/main/app/src/components/ScoreChart.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Scatter } from "react-chartjs-2";
+
+function ScoreChart({ chartData }) {
+  return (
+    <div className="chart-container">
+      <Scatter
+        data={chartData}
+        options={{
+          showLine: true,
+          scales: {
+            y: {
+              min: 0,
+              suggestedMax: 300,
+              border: {
+                display: false
+              },
+              grid: {
+                display: false
+              },
+              ticks: {
+                display: false
+              }
+            },
+            x: {
+              min: 0,
+              suggestedMax: 12,
+              border: {
+                display: false
+              },
+              grid: {
+                display: false
+              },
+              ticks: {
+                display: false
+              }
+            }
+          }
+        }}
+      />
+    </div>
+  );
+}
+export default ScoreChart;
---
+++ src/main/app/src/utils/Data.js
@@ -0,0 +1,26 @@
+export const Data = [
+  {
+    time: 0,
+    total: 0
+  },
+  {
+    time: 32.0/3600,
+    total: 7
+  },
+  {
+    time: 436.0/3600,
+    total: 14
+  },
+  {
+    time: 1793.0/3600,
+    total: 17
+  },
+  {
+    time: 2957.0/3600,
+    total: 24
+  },
+  {
+    time: 1,
+    total: 24
+  }
+];
