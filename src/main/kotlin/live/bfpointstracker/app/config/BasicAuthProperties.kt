0a686373af0ca286c0f8de02ca485e006c1e639c
David Dellsperger <david.dellsperger@gmail.com>
2023-09-08 19:49:47 -0500

    Move to Basic Auth configured in the application.yml

---
+++ src/main/kotlin/live/bfpointstracker/app/config/BasicAuthProperties.kt
@@ -0,0 +1,28 @@
+package live.bfpointstracker.app.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+
+@ConfigurationProperties(prefix = "auth")
+class BasicAuthProperties {
+  private lateinit var users: Map<String, UserDetail>
+
+  fun getUserDetails(): Set<UserDetails> {
+    return users.entries
+      .map { entry ->
+        User.withUsername(entry.key)
+          .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+          .password(entry.value.password)
+          .roles(entry.value.role)
+          .build()
+      }
+      .toSet()
+  }
+
+  class UserDetail {
+    lateinit var password: String
+    lateinit var role: String
+  }
+}
